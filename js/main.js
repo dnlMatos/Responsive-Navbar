e0924a83bdc88526ca1c2233c907a61dbedb53ff
Danilo Matos <georginedanilo@gmail.com>
2021-05-04 18:38:35 -0300

    ausencia animação hambuger

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let menuHamb = document.querySelector(".hamb");
+let listaMenu = document.querySelector('.listaMenu');
+
+menuHamb.addEventListener('click', ()=>{
+    listaMenu.classList.toggle('show');    
+})
+
